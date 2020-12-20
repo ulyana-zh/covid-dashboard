6fa7a468ba9c60c42707f9bea6a925beadb3caaa
anDevRom <70050410+anDevRom@users.noreply.github.com>
2020-12-20 13:24:19 +0300

    feat: add search country input

--- covid-dashboard/src/js/state.js
+++ covid-dashboard/src/js/state.js
@@ -2,6 +2,18 @@ const state = {
   isGlobal: true,
   currentCountry: null,
   currentListMode: 'cases',
+  searchValue: '',
+  currentList: null,
+
+  //  получить данные для всех стран из стора
+  //  отсортировать согласно текущему моду
+  //  отдать массив соответствующий поисковому запросу
+
+  // getDataFromStore() {
+  //   store.getAllCountriesData().then(() => {
+  //     ;
+  //   });
+  // }
 };
 
 export default state;
